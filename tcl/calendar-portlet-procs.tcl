9f9bc801f8be568839079a401a259df69a1bc391
Ben Adida <ben@openforce.net>
2001-12-09 16:47:07 +0000

    conflict resolution

--- tcl/calendar-portlet-procs.tcl
+++ tcl/calendar-portlet-procs.tcl
@@ -31,19 +31,19 @@ namespace eval calendar_portlet {
     }
 
     ad_proc -public add_self_to_page { 
-	page_id 
+	portal_id 
 	calendar_id
     } {
 	Adds a calendar PE to the given page with the community_id.
     
 	@return element_id The new element's id
-	@param page_id The page to add self to
+	@param portal_id The page to add self to
 	@param community_id The community with the folder
 	@author arjun@openforce.net
 	@creation-date Sept 2001
     } {
 	# Tell portal to add this element to the page
-	set element_id [portal::add_element $page_id [my_name]]
+	set element_id [portal::add_element $portal_id [my_name]]
 	
 	# The default param "calendar_id" must be configured
 	set key "calendar_id"
@@ -429,7 +429,7 @@ namespace eval calendar_portlet {
     } {
 	  Removes a calendar PE from the given page
     
-	  @param page_id The page to remove self from
+	  @param portal_id The page to remove self from
 	  @param community_id
 	  @author arjun@openforce.net
 	  @creation-date Sept 2001
@@ -464,28 +464,28 @@ namespace eval calendar_portlet {
     }
     
     ad_proc -public make_self_available {
- 	page_id
+ 	portal_id
     } {
  	Wrapper for the portal:: proc
  	
- 	@param page_id
+ 	@param portal_id
  	@author arjun@openforce.net
  	@creation-date Nov 2001
     } {
  	portal::make_datasource_available \
- 		$page_id [portal::get_datasource_id [my_name]]
+ 		$portal_id [portal::get_datasource_id [my_name]]
     }
 
     ad_proc -public make_self_unavailable {
-	page_id
+	portal_id
     } {
 	Wrapper for the portal:: proc
 	
-	@param page_id
+	@param portal_id
 	@author arjun@openforce.net
 	@creation-date Nov 2001
     } {
 	portal::make_datasource_unavailable \
-		$page_id [portal::get_datasource_id [my_name]]
+		$portal_id [portal::get_datasource_id [my_name]]
     } 
 }
