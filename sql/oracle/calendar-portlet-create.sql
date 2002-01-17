40bc25036272583cc8efc220537c1f8d73f75968
Yonatan Feldman <yon@milliped.com>
2002-01-17 22:12:22 +0000

    link hide stuff

--- sql/oracle/calendar-portlet-create.sql
+++ sql/oracle/calendar-portlet-create.sql
@@ -62,6 +62,15 @@ begin
 	value => 'f'
 );	
 
+  -- link_hideable_p 
+  portal_datasource.set_def_param (
+	datasource_id => ds_id,
+	config_required_p => 't',
+	configured_p => 't',
+	key => 'link_hideable_p',
+	value => 'f'
+);	
+
 
   -- calendar-specific params
 
