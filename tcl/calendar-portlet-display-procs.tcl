41d5c08a7c0c60771012e37e497e1b4db3fdea1f
Ben Adida <ben@openforce.net>
2002-04-17 14:43:41 +0000

    added display procs

---
+++ tcl/calendar-portlet-display-procs.tcl
@@ -0,0 +1,40 @@
+#
+#  Copyright (C) 2001, 2002 OpenForce, Inc.
+#
+#  This file is part of dotLRN.
+#
+#  dotLRN is free software; you can redistribute it and/or modify it under the
+#  terms of the GNU General Public License as published by the Free Software
+#  Foundation; either version 2 of the License, or (at your option) any later
+#  version.
+#
+#  dotLRN is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+#  details.
+#
+
+# calendar-portlet/tcl/calendar-portlet-display-procs.tcl
+
+ad_library {
+
+Procedures to support the calendar portlet display
+
+Copyright Openforce, Inc.
+Licensed under GNU GPL v2 
+
+@creation-date April 15, 2002
+@author ben@openforce.net
+@cvs-id $Id$
+
+}
+
+namespace eval calendar_portlet_display {
+    
+    ad_proc -public get_url_stub {
+        calendar_id
+    } {
+        return [portal::mapping::get_url -object_id $calendar_id]
+    }
+    
+}
