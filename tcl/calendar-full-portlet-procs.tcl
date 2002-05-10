88d8245f177e7cbfbd5a0268d17536c7a410e96d
Arjun Sanyal <arjun@openforce.net>
2002-05-10 03:58:26 +0000

    minor fixes for missing titles, params

--- tcl/calendar-full-portlet-procs.tcl
+++ tcl/calendar-full-portlet-procs.tcl
@@ -40,7 +40,10 @@ namespace eval calendar_full_portlet {
 
     ad_proc -public get_pretty_name {
     } {
-        return [ad_parameter "full_portlet_pretty_name" [my_package_key]]
+        return [oacs_util::parameter \
+                -key "full_portlet_pretty_name" \
+                -package_key [my_package_key]
+        ]
     }
 
     ad_proc -public link {
--- tcl/calendar-portlet-procs.tcl
+++ tcl/calendar-portlet-procs.tcl
@@ -38,7 +38,10 @@ namespace eval calendar_portlet {
 
     ad_proc -public get_pretty_name {
     } {
-        return [ad_parameter "pretty_name" [my_package_key]]
+        return [oacs_util::parameter \
+                -key "pretty_name" \
+                -package_key [my_package_key]
+        ]
     }
 
     ad_proc -public link {
