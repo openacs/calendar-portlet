c5e7457694e1085b9fa587e3439f86b7833d8085
Don Baccus <dhogaza@pacifier.com>
2007-03-12 03:13:31 +0000

    Bumped version number and added callback.

---
+++ tcl/apm-callback-procs.tcl
@@ -0,0 +1,24 @@
+ad_library {
+    
+    APM callback procs for calendar-portlet
+
+    @author Don Baccus (dhogaza@pacifier.com)
+}
+
+namespace eval calendar-portlet {}
+
+ad_proc -private calendar-portlet::after_upgrade {
+    {-from_version_name:required}
+    {-to_version_name:required}
+} {
+    After upgrade callback for calendar-portlet.
+} {
+    apm_upgrade_logic \
+        -from_version_name $from_version_name \
+        -to_version_name $to_version_name \
+        -spec {
+            2.3.0d1 2.3.0d2 {
+                db_dml update_portal_datasources {}
+            }
+    }
+}
